90ef1a50d2280ee51ea18d1c8c99c3518be4b8b8
Rakib <rakibulh170@gmail.com>
2021-03-03 01:15:48 +0600

    remove media

--- ebaybdApp/urls.py
+++ ebaybdApp/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
 
@@ -22,4 +20,3 @@ urlpatterns = [
     path('donate_blood_register/',views.doner_reg_view, name='doner_reg'),
 
 ]
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
